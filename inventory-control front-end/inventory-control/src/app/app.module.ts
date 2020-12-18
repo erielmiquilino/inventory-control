07a22bf1e845261b1f6c0a2becbb6a914bce6096
Eriel Miquilino <erielmiquilino@hotmail.com>
2020-12-18 02:07:11 -0300

    implementação inicial da tela de cadastro de vendedor (Seller)

--- inventory-control front-end/inventory-control/src/app/app.module.ts
+++ inventory-control front-end/inventory-control/src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SellerFormComponent } from './features/seller/seller-form/seller-form.component';
+import { InputTextModule } from 'primeng/inputtext';
+import {ReactiveFormsModule} from '@angular/forms';
+import {CalendarModule} from 'primeng/calendar';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {registerLocaleData} from '@angular/common';
+import ptBr from '@angular/common/locales/pt';
+
+registerLocaleData(ptBr);
 
 @NgModule({
   declarations: [
     AppComponent,
     SellerFormComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        InputTextModule,
+        CalendarModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+    ],
+  providers: [{provide: LOCALE_ID, useValue: 'pt'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
