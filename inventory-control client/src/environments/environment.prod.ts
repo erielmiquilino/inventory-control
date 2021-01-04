4e60c2a76718ca68f61885c586550fd0ddf31821
Eriel Miquilino <erielmiquilino@hotmail.com>
2021-01-03 23:27:16 -0300

    alterações para executar o forntend em modo de produção no container

--- inventory-control client/src/environments/environment.prod.ts
+++ inventory-control client/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   locality: 'https://servicodados.ibge.gov.br/api/v1/localidades',
   viaCep: 'https://viacep.com.br/ws',
+  application: 'https://inventory-control-application-qah7safrea-rj.a.run.app',
   production: true
 };
